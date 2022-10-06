5c3cecb8463ae3ba599a6702344835f9b34631d2
Stévan Corre <stevancorre@protonmail.com>
2022-10-06 11:08:04 +0200

    [products] validate product creation form

--- src/main/java/com/stevancorre/cda/gui/products/CreateProductOptionPanel.java
+++ src/main/java/com/stevancorre/cda/gui/products/CreateProductOptionPanel.java
@@ -5,8 +5,7 @@ import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.text.NumberFormat;
 
-import static com.stevancorre.cda.gui.GUIUtils.makeLabel;
-import static com.stevancorre.cda.gui.GUIUtils.showError;
+import static com.stevancorre.cda.gui.GUIUtils.*;
 
 class CreateProductOptionPanel extends JPanel {
     private final JTextField nameField;
@@ -16,11 +15,9 @@ class CreateProductOptionPanel extends JPanel {
     CreateProductOptionPanel() {
         super(new GridLayout(0, 1));
 
-        final NumberFormat paymentFormat = NumberFormat.getCurrencyInstance();
-
         this.nameField = new JTextField();
-        this.priceField = new JTextField();
-        this.quantityField = new JTextField();
+        this.priceField = makeDoubleTextField();
+        this.quantityField = makeIntTextField();
 
         add(makeLabel("Name", l -> l.setLabelFor(this.nameField)));
         add(this.nameField);
@@ -41,17 +38,24 @@ class CreateProductOptionPanel extends JPanel {
                 JOptionPane.PLAIN_MESSAGE);
 
         try {
+            final String name = this.nameField.getText();
+            final double price = Double.parseDouble(this.priceField.getText());
+            final int quantity = Integer.parseInt(this.quantityField.getText());
+
+            if(name.isBlank())
+                throw new Exception();
+
             if (result == JOptionPane.OK_OPTION)
                 return new CreateProductData(
-                        this.nameField.getText(),
-                        Double.parseDouble(this.priceField.getText()),
-                        Integer.parseInt(this.quantityField.getText())
+                        name,
+                        price,
+                        quantity
                 );
             else
                 return null;
         }
-        catch (NumberFormatException e) {
-            showError("Invalid number format");
+        catch (Exception e) {
+            showError("Invalid input");
 
             return prompt();
         }
