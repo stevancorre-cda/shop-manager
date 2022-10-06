c6df8b6a1190ec76830361c347b4d10909b19283
Stévan Corre <stevancorre@protonmail.com>
2022-10-06 10:54:46 +0200

    [orders] fix edit title and display date

--- src/main/java/com/stevancorre/cda/gui/orders/edit/EditOrderOptionPanel.java
+++ src/main/java/com/stevancorre/cda/gui/orders/edit/EditOrderOptionPanel.java
@@ -9,16 +9,21 @@ import static com.stevancorre.cda.gui.GUIUtils.makeLabel;
 
 public class EditOrderOptionPanel extends JPanel {
     public EditOrderOptionPanel(final Order order) {
-
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setAlignmentX(LEFT_ALIGNMENT);
 
+        final JTextField dateField = new JTextField() {{
+            setText(order.getFormattedDate());
+            setEnabled(false);
+        }};
         final JTextField clientField = new JTextField() {{
             setText(order.getClient().getFullName());
             setEnabled(false);
         }};
         final OrderProductsList productsList = new OrderProductsList(order.getProducts(), order.getStatus() == OrderStatus.Finalized);
 
+        add(makeLabel("Date", l -> l.setLabelFor(dateField)));
+        add(dateField);
         add(makeLabel("Client", l -> l.setLabelFor(clientField)));
         add(clientField);
         add(productsList);
@@ -28,7 +33,7 @@ public class EditOrderOptionPanel extends JPanel {
         JOptionPane.showConfirmDialog(
                 null,
                 this,
-                "Make new order",
+                "Edit order",
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.PLAIN_MESSAGE);
     }
